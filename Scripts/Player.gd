ef6117f55e323f1471230e243bdf67b295daf840
juan tacchi <jptacchip@gmail.com>
2023-04-09 17:47:31 -0400

    feat: add simple walk and iddle animation to player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-var Food = null 
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
