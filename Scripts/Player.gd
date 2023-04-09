70311707da8a2dd2e45898f245d5abb490ec8c29
juan tacchi <jptacchip@gmail.com>
2023-04-09 18:54:05 -0400

    feat: add animation when moving to the player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,8 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
-
+@onready var anim = $AnimationPlayer
+@export var BtnDer = "ui_right"
+@export var BtnIzq = "ui_left"
+@export var BtnArr = "ui_up"
+@export var BtnAbj = "ui_down"
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,8 +19,19 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
 
-	var dir = Vector2(Input.get_axis("ui_left", "ui_right"),Input.get_axis("ui_up", "ui_down"))
+	var dir = Vector2(Input.get_axis(BtnIzq, BtnDer),Input.get_axis(BtnArr, BtnAbj))
 	velocity = dir*SPEED
+	
+	
+	if (velocity.length() > 0 ):
+		anim.play("run")
+		if(velocity.x > 0):
+			$Sprite2D.flip_h = false
+		else:
+			$Sprite2D.flip_h = true
+	else:
+		anim.play("Idle")
+		
 	
 	#if direction:
 	#	velocity.x = direction * SPEED
