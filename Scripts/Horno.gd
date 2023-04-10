a15207b17ce9ed1967492bf88cb02a0cde9d3a18
juan tacchi <jptacchip@gmail.com>
2023-04-09 20:26:35 -0400

    feat: add second player working

--- Scripts/Horno.gd
+++ Scripts/Horno.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+@onready var Anim = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func cocinarIngrediente():
+	pass
+
+func entrgearIngrediente():
+	pass
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player1"):
+		body.entregaIngrediente(self)
+	
--- Scripts/Masa.gd
+++ Scripts/Masa.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
