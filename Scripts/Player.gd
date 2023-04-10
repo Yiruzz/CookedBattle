508d235f953661c31ccfddcef115b8e3b0ef48fd
juan tacchi <jptacchip@gmail.com>
2023-04-09 22:57:39 -0400

    Fix: fix the last commit

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -89,5 +89,5 @@ func _physics_process(delta):
 func _on_area_2d_body_entered(body):
 	if !body.is_in_group(self.get_groups()[0]):
 		if body.is_in_group("Players"):
-			body.recdibirDaño()
+			body.recibirDaño()
 		
