ee65e53f582970d476ed3ca67150a471576c03cb
Yiruzz <arturofks@gmail.com>
2023-06-23 04:36:27 -0400

    Feat: Added recipies menu that shows the possible recipies

--- beginScreen.gd
+++ beginScreen.gd
@@ -3,7 +3,6 @@ extends MarginContainer
 @onready var play = %Play
 @onready var credits = %Credits
 @onready var exit = %Exit
-var modo = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,7 +14,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/load_menu.tscn")
 
 func _on_credits_pressed():
 	pass
---
+++ load_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var play = $"."
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play.pressed.connect(_on_play_pressed)
+	play.grab_focus()
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
